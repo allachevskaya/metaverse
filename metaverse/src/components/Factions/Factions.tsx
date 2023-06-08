48c7c81e38d7f9b34b6de88b779a1a4acdd2c3cd
allachevskaya <alprel@list.ru>
2023-06-08 19:52:51 +0300

    add paralax effect

---
+++ metaverse/src/components/Factions/Factions.tsx
@@ -0,0 +1,8 @@
+
+
+const Factions = () =>{
+    return(
+        <h1>Factions</h1>
+    )
+}
+export default Factions
--- metaverse/src/pages/index.tsx
+++ metaverse/src/pages/index.tsx
@@ -1,16 +1,35 @@
+import Factions from '@/components/Factions/Factions'
 import Main from '@/components/Main/Main'
 import Layout from '@/components/layout/Layout'
 
 import Head from 'next/head'
+import { Parallax, ParallaxProvider } from 'react-scroll-parallax'
 
 export default function Home() {
+
+
+
   return (
-    <>
+    <ParallaxProvider>
       <Layout title={'DUSKTOPIA'} description={''} keywords={''} >
 
-        <main>
-          <Main/>
-         
+        <main >
+          <Parallax  speed={-15}>
+            <Main />
+          </Parallax>
+          <Parallax  speed={15}>
+            <Main />
+          </Parallax>
+
+          <Parallax  speed={-15}>
+            <Main />
+          </Parallax>
+          <Parallax  speed={15}>
+            <Main />
+          </Parallax>
+        
+          {/* <Factions/> */}
+
 
         </main>
 
@@ -18,6 +37,6 @@ export default function Home() {
 
       </Layout>
 
-    </>
+    </ParallaxProvider>
   )
 }
